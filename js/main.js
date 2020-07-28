34d1b80fffbab4029069e36179018d1b57ec14d9
Sergey Chin <the.sergiusac@gmail.com>
2020-07-28 21:28:30 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,211 @@
+
+var aromas = {
+    'man': [
+        {
+            'title': 'Armani — Acqua Di Gio',
+            'desc': 'Легкий и невесомый аромат создан для тех, кто мечтает о жизни на морском побережье. Освежающая волна бергамота, лайма, зеленого мандарина и лимона стремительно сменяется цветочно-древесным коктейлем из цикламена, фрезии и кориандра, оставляя после себя сладковатый шлейф из пачули и мускуса.',
+            'image_url': 'img/perfume_man_1.jpg'
+        },
+        {
+            'title': 'Arabica By Meilleur',
+            'desc': 'Arabica by Meilleur — уникальный аромат с восточными мотивами. Яркая, бодрящая композиция создана для страстных и темпераментных мужчин и женщин. Терпкий, но чувственный шлейф надолго запомнится вашим пассажирам.',
+            'image_url': 'img/perfume_man_2.jpg'
+        },
+        {
+            'title': 'Christian Dior - Dior Homme Sport',
+            'desc': 'Ноmmе Sport — свежий, энергичный аромат с успокаивающим древесным шлейфом. Главная нота запаха — благородный тосканский ирис. Он смягчает терпкое звучание имбиря и подчеркивает фруктовую свежесть сицилийского цитрона. Подойдет для уверенных в себе и своих силах мужчин.',
+            'image_url': 'img/perfume_man_3.jpg'
+        },
+        {
+            'title': 'Versace — Man Eau Fraiche',
+            'desc': 'Яркий, свежий аромат. В его сердце — бодрящие нотки тархуна, терпкий запах шалфея и древесные оттенки белого кедра. Создан для сильных и целеустремленных мужчин, которые умеют ценить собственную свободу и независимость.',
+            'image_url': 'img/perfume_man_4.jpg'
+        },
+        {
+            'title': 'Clinique — Happy',
+            'desc': 'Happy создан для мужчин, которые умеют наслаждаться жизнью, любят быструю езду и приключения. Бодрящие цитрусовые ноты апельсина, мандарина и грейпфрута заряжают энергией, а древесные ноты кедра, средиземноморского кипариса и дерева гуаяк придают уверенность в своих силах.',
+            'image_url': 'img/perfume_man_5.jpg'
+        },
+        {
+            'title': 'Paco Rabanne — Black Xs',
+            'desc': 'Аромат, играющий на контрастах. Искрящаяся свежесть шалфея и лимона сменяется сладкими и терпкими нотами пачули и корицы. Завершают симфонию древесные аккорды серой амбры, пачули и черного дерева. Для решительных и брутальных мужчин, которые скрывают свою чувственную натуру.',
+            'image_url': 'img/perfume_man_6.jpg'
+        },
+        {
+            'title': 'Carolina Herrera - 212 Vip Men',
+            'desc': '212 VIP Men — взрывной коктейль для любителя вечеринок. В составе — водка, ледяная мята, освежающий лайм и будоражащие специи. Пьянящая композиция завершается чувственными, но мужественными нотками королевского дерева, пикантной амбры и бобов тонка. Подойдет для харизматичного, веселого и стильного жителя мегаполиса.',
+            'image_url': 'img/perfume_man_7.jpg'
+        },
+        {
+            'title': 'Montale - Boise Fruite',
+            'desc': 'Букет из лесных цветов и яркие свежие нотки цитрусовых даже в морозный зимний день напомнят о лете. Может меняться в зависимости от погоды и времени суток. Яркий, терпкий, насыщенный аромат подойдет уверенным в себе женщинам и мужчинам.',
+            'image_url': 'img/perfume_man_8.jpg'
+        },
+        {
+            'title': 'Paco Rabanne - 1 Million',
+            'desc': 'Свежесть грейпфрута, мяты и красного мандарина стремительно сменяется насыщенными сердечными нотами розы, корицы и пряностей. Завершает композицию яркий и выразительный кожаный аккорд. Чувственный и непредсказуемый аромат подчеркнет вашу мужественность и статус. Идеально подойдет к кожаному салону.',
+            'image_url': 'img/perfume_man_9.jpg'
+        },
+        {
+            'title': 'Lacoste — White',
+            'desc': 'Чистый, свежий и ненавязчивый аромат. Встречает бодрящим коктейлем из розмарина, кардамона и грейпфрута, раскрывается сладковатыми нотками иланг-иланга и оставляет после себя легкий древесный шлейф. Подойдет для тех, кто не любит слишком яркие запахи в салоне автомобиля. Самый популярный аромат в мужской линейке Meilleur 2017 года.',
+            'image_url': 'img/perfume_man_10.jpg'
+        }
+    ],
+    'woman': [
+        {
+            'title': 'Lacoste — Pour Femme',
+            'desc': 'Pour Femme от Lacoste создан для молодых, свободных женщин, уверенных в себе и в своем будущем. Знакомство с ароматом начинается с яркого запаха красного перца и фиолетовой фрезии. Затем вас ждет завораживающий букет из белого гелиотропа, иранского жасмина, гибискуса и болгарской розы. И в завершении — чувственный шлейф из кедра, сандала и мускуса.',
+            'image_url': 'img/perfume_woman_1.jpg'
+        },
+        {
+            'title': 'Armani — Si',
+            'desc': 'Глубокий и насыщенный шипровый аромат создан для сильной, уверенной в себе женщины. В его основе — три ярких аккорда: черная смородина, майская роза и едва уловимые нотки мускусного дерева.',
+            'image_url': 'img/perfume_woman_2.jpg'
+        },
+        {
+            'title': 'Chanel — Chance',
+            'desc': 'Выбор для чувственных, ярких и эффектных женщин. В самом сердце Chance — яркие аккорды розового перца, освежающего лимона и нежного жасмина. Легкая шипрово-цветочная композиция сменяется соблазнительными нотками томного пачули, пьянящего ириса и сладкого гиацинта.',
+            'image_url': 'img/perfume_woman_3.jpg'
+        },
+        {
+            'title': 'Paco Rabanne — Lady Million',
+            'desc': 'Lady Million — свежий цветочный аромат с древесным оттенком. За свежестью цитрусовых следуют нежные женственные ноты гардении и гиацинта. Но главный секрет притягательности аромата — сочетание сладкого меда с чувственными нотами пачули — раскрывается последним. Подойдет для любителей роскоши и голливудского гламура.',
+            'image_url': 'img/perfume_woman_4.jpg'
+        },
+        {
+            'title': 'Christian Dior — J\'Adore',
+            'desc': 'Насыщенный чувственный цветочный аромат идеально подчеркнет статус вашего автомобиля. В нотах сердца J\'adore раскрывается букет из черной розы и нежного ландыша. Оттеняют композицию легкие деликатные нотки жасмина и сладковатый коктейль из ванили, мускуса и сандала.',
+            'image_url': 'img/perfume_woman_5.jpg'
+        },
+        {
+            'title': 'Escada — Pacific Paradise',
+            'desc': 'Игривый коктейль из сочных тропических фруктов и сладких нот сахарного тростника, сандалы и мускуса перенесет вас и ваших пассажиров на шумную пляжную вечеринку. Аромат понравится веселым девушкам, которые умеют наслаждаться жизнью.',
+            'image_url': 'img/perfume_woman_6.jpg'
+        },
+        {
+            'title': 'Dkny - Be Delicious',
+            'desc': 'Гармоничная композиция из нежных цветов и свежих фруктов. Первые аккорды — душистая магнолия и кислый грейпфрут, им вторит благоухающий букет из туберозы, ландыша, фиалки и розы. Завершает композицию сочное зеленое яблоко. Идеальный аромат для открытой, любящей жизнь девушки.',
+            'image_url': 'img/perfume_woman_7.jpg'
+        },
+        {
+            'title': 'Givenchy - Ange Ou Demon Le Secret',
+            'desc': 'Нежный, но чувственный, легкий, но манящий — Ange Ou Demon Le Secret идеально балансирует на грани. Фруктово-цветочный аромат раскрывается освежающими нотками чайных листьев и бергамота, нежным букетом из жасмина и розы, оставляя после себя едва уловимый шлейф из пачули и мускуса. Лучший выбор для женщин, в которых сочетается нежность и взрывной характер.',
+            'image_url': 'img/perfume_woman_8.jpg'
+        },
+        {
+            'title': 'Salvatore Ferragamo - Incanto Shine',
+            'desc': 'Яркая цветочно-фруктовая композиция будет напоминать вам о летнем отдыхе и воскресных прогулках. Incanto Shine встречает тропическими нотками ананаса и маракуйи и раскрывается чарующими ароматами персика, амбры и розового пиона. Подойдет для веселых, любящих жизнь девушек.',
+            'image_url': 'img/perfume_woman_9.jpg'
+        },
+        {
+            'title': 'Escada - Taj Sunset',
+            'desc': 'Taj Sunset — это сладкий тропический коктейль из манго, кокоса и нектаринов. Аромат пробуждает воспоминания об отпуске у океана: белоснежном песке, шелесте пальм и душистом бризе. Идеально подойдет для романтичных  женщин.',
+            'image_url': 'img/perfume_woman_10.jpg'
+        }
+    ]
+}
+
+var headerPointer = 0;
+var forWhom = 'man';
+var mainAssortmentPointer = 0;
+
+function setHeaderOrderInfo(point) {
+    if (point >= 0 && point <= 9) {
+        $('.header-order__title p').text(aromas['woman'][point].title);
+        $('.header-order__image img').attr('src', aromas['woman'][point].image_url);
+    } else if (point >= 10 && point <= 19) {
+        $('.header-order__title p').text(aromas['man'][point - 10].title);
+        $('.header-order__image img').attr('src', aromas['man'][point - 10].image_url);
+    }
+}
+
+function setMainAssortmentInfo(point, forWhom, isMobile) {
+    if (isMobile) {
+        $('.aromas-m-info-block__title h4').text(aromas[forWhom][point].title);
+        $('.aromas-m-info-block__desc p').text(aromas[forWhom][point].desc);
+        $('.aromas-m-info-block__image img').attr('src', aromas[forWhom][point].image_url);
+    } else {
+        $('.info-block__title h4').text(aromas[forWhom][point].title);
+        $('.info-block__desc p').text(aromas[forWhom][point].desc);
+        $('.aromas-info__image img').attr('src', aromas[forWhom][point].image_url);
+    }
+}
+
+$(document).ready(function () {
+
+    setHeaderOrderInfo(headerPointer);
+
+    $('.header-order__row_left img').on('click', function () {
+        headerPointer--;
+        if (headerPointer < 0) {
+            headerPointer = 19;
+        }
+        setHeaderOrderInfo(headerPointer);
+    });
+
+    $('.header-order__row_right img').on('click', function () {
+        headerPointer = (headerPointer + 1) % 20;
+        setHeaderOrderInfo(headerPointer);
+    });
+
+    $('.aromas-man .aromas__btn button').on('click', function () {
+        forWhom = 'man';
+        mainAssortmentPointer = 0;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, false);
+    })
+
+    $('.aromas-woman .aromas__btn button').on('click', function () {
+        forWhom = 'woman';
+        mainAssortmentPointer = 0;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, false);
+    })
+
+    $('.aromas-info__row_left img').on('click', function () {
+        mainAssortmentPointer--;
+        if (mainAssortmentPointer < 0) {
+            mainAssortmentPointer = 9;
+        }
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, false);
+    });
+
+    $('.aromas-info__row_right img').on('click', function () {
+        mainAssortmentPointer = (mainAssortmentPointer + 1) % 10;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, false);
+    });
+
+    $('.aromas-m-btns-man button').on('click', function () {
+        forWhom = 'man';
+        mainAssortmentPointer = 0;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, true);
+    });
+
+    $('.aromas-m-btns-woman button').on('click', function () {
+        forWhom = 'woman';
+        mainAssortmentPointer = 0;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, true);
+    });
+
+    $('.aromas-m-info__row_left img').on('click', function () {
+        mainAssortmentPointer--;
+        if (mainAssortmentPointer < 0) {
+            mainAssortmentPointer = 9;
+        }
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, true);
+    });
+
+    $('.aromas-m-info__row_right img').on('click', function () {
+        mainAssortmentPointer = (mainAssortmentPointer + 1) % 10;
+        setMainAssortmentInfo(mainAssortmentPointer, forWhom, true);
+    });
+
+    $('.aromas-m-info-block__desc span').on('click', function () {
+        if ($('.aromas-m-info-block__desc_toggle')[0].hidden) {
+            $('.aromas-m-info-block__desc span').text('Скрыть описание');
+            $('.aromas-m-info-block__desc_toggle')[0].hidden = false;
+        } else {
+            $('.aromas-m-info-block__desc span').text('Показать описание');
+            $('.aromas-m-info-block__desc_toggle')[0].hidden = true;
+        }
+    })
+});
